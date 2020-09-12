c49ab994e3a1093e47872672306f34aee269fb86
aaneitchik <aaneitchik@gmail.com>
2020-09-12 20:22:18 +0300

    Remove image classification logic from ImageUpload

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,49 @@
 import './app.css';
 
-import React from 'react';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import React, { useEffect, useRef, useState } from 'react';
 
 import ImageUpload from './components/image-upload';
+import { getDogBreed, preloadModel } from './get-dog-breed.worker';
 
 const App = (): JSX.Element => {
+  const modelPromise = useRef<Promise<mobilenet.MobileNet> | null>(null);
+  const [classificationError, setClassificationError] = useState<string | null>(
+    null,
+  );
+
+  /**
+   * Start loading the model right away in the web worker,
+   * so it's there when we need it
+   */
+  useEffect((): void => {
+    // No sense in moving this outside, it's a crucial part of this effect
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    const loadModel = async (): Promise<void> => {
+      modelPromise.current = preloadModel();
+    };
+
+    try {
+      // This is the way to call an async function in useEffect
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      loadModel();
+    } catch {
+      setClassificationError(`Couldn't load model`);
+    }
+  }, []);
+
+  const handleImageLoad = async (
+    imageUrl: string,
+    width: number,
+    height: number,
+  ): Promise<void> => {
+    await getDogBreed(imageUrl, width, height, modelPromise.current);
+  };
+
   return (
     <div className="app">
-      <ImageUpload />
+      <ImageUpload onImageLoad={handleImageLoad} />
+      {!!classificationError && <div>{classificationError}</div>}
     </div>
   );
 };
--- src/components/image-upload.tsx
+++ src/components/image-upload.tsx
@@ -1,38 +1,17 @@
 import './image-upload.css';
 
-import * as mobilenet from '@tensorflow-models/mobilenet';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
-import { getDogBreed, preloadModel } from './get-dog-breed.worker';
+interface ImageUploadProps {
+  onImageLoad: (imageUrl: string, width: number, height: number) => void;
+}
 
-const ImageUpload: React.FC = (): JSX.Element => {
-  const modelPromise = useRef<Promise<mobilenet.MobileNet> | null>(null);
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  onImageLoad,
+}: ImageUploadProps): JSX.Element => {
   const imageReference = useRef<HTMLImageElement | null>(null);
   const inputReference = useRef<HTMLInputElement | null>(null);
   const [uploadedImageUrl, setUploadedImageUrl] = useState<string | null>(null);
-  const [classificationError, setClassificationError] = useState<string | null>(
-    null,
-  );
-
-  /**
-   * Start loading the model right away in the web worker,
-   * so it's there when we need it
-   */
-  useEffect((): void => {
-    // No sense in moving this outside, it's a crucial part of this effect
-    // eslint-disable-next-line unicorn/consistent-function-scoping
-    const loadModel = async (): Promise<void> => {
-      modelPromise.current = preloadModel();
-    };
-
-    try {
-      // This is the way to call an async function in useEffect
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      loadModel();
-    } catch {
-      setClassificationError(`Couldn't load model`);
-    }
-  }, []);
 
   const handleImageChange = async (): Promise<void> => {
     if (!inputReference?.current?.files?.[0]) {
@@ -50,12 +29,7 @@ const ImageUpload: React.FC = (): JSX.Element => {
 
     // Get original dimensions, it improves classification
     const { naturalWidth, naturalHeight } = imageReference.current;
-    await getDogBreed(
-      uploadedImageUrl,
-      naturalWidth,
-      naturalHeight,
-      modelPromise.current,
-    );
+    onImageLoad(uploadedImageUrl, naturalWidth, naturalHeight);
   };
 
   return (
@@ -84,8 +58,6 @@ const ImageUpload: React.FC = (): JSX.Element => {
       <label className="image-upload__btn" htmlFor="image-upload">
         Upload image
       </label>
-
-      {!!classificationError && <div>{classificationError}</div>}
     </div>
   );
 };
