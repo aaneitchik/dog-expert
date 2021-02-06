08f3dd9b0097d9c45c6174c92c207756f4b76656
Anna Aneychik <anna.aneychik@ridedott.com>
2021-02-06 20:49:05 +0100

    100% coverage for src/utils

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,19 @@ module.exports = {
   moduleNameMapper: {
     '\\.css$': require.resolve('./test/style-mock.ts'),
   },
+  collectCoverageFrom: ['**/src/**/*.{ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      statements: 0,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+    },
+    './src/utils': {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
 };
--- src/utils/__tests__/get-breed-url.ts
+++ src/utils/__tests__/get-breed-url.ts
@@ -6,3 +6,42 @@ test('returns breed if all possible breeds are not provided', () => {
 
   expect(getBreedUrl(breed, allBreeds)).toStrictEqual(breed);
 });
+
+test('returns breed if there are no sub-breeds', () => {
+  const breed = 'beagle';
+  const allBreeds = { beagle: [] };
+
+  expect(getBreedUrl(breed, allBreeds)).toStrictEqual(breed);
+});
+
+test('returns breed without spaces if it is an available breed', () => {
+  const breed = 'german shepherd';
+  const allBreeds = { germanshepherd: [] };
+
+  expect(getBreedUrl(breed, allBreeds)).toStrictEqual('germanshepherd');
+});
+
+test('returns master/sub-breed', () => {
+  const breed = 'french bulldog';
+  const allBreeds = {
+    bulldog: ['boston', 'english', 'french'],
+  };
+
+  expect(getBreedUrl(breed, allBreeds)).toStrictEqual('bulldog/french');
+});
+
+test('returns sub-breed/master', () => {
+  const breed = 'australian shepherd';
+  const allBreeds = {
+    australian: ['shepherd'],
+  };
+
+  expect(getBreedUrl(breed, allBreeds)).toStrictEqual('australian/shepherd');
+});
+
+test('returns breed if none of transformations to find it in the breeds worked', () => {
+  const breed = 'cardigan welsh corgi';
+  const allBreeds = { beagle: [] };
+
+  expect(getBreedUrl(breed, allBreeds)).toStrictEqual('cardigan welsh corgi');
+});
