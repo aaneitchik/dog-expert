a7f5f88a8d3816e9ebfe2f124e6a1dce8c09907a
Anna Aneychik <anna.aneychik@ridedott.com>
2021-02-21 20:31:36 +0100

    Cover visible message test case for Loader

--- src/components/__tests__/loader.tsx
+++ src/components/__tests__/loader.tsx
@@ -25,9 +25,17 @@ test('renders loader with className', () => {
   expect(screen.getByRole('progressbar')).toHaveClass('loader--default');
 });
 
-test('renders loader with message', () => {
+test('renders loader with accessible invisible message', () => {
   render(<Loader message="Classifying..." />);
 
   const loader = screen.getByRole('progressbar', { name: 'Classifying...' });
   expect(loader).toBeVisible();
+});
+
+test('renders loader with accessible visible message', () => {
+  render(<Loader message="Classifying..." isMessageVisible />);
+
+  const loader = screen.getByRole('progressbar', { name: 'Classifying...' });
+  expect(loader).toBeVisible();
+  expect(loader).toHaveTextContent('Classifying...');
 });
