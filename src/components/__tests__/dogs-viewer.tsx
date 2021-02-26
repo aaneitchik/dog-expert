6fa48701f531f12db14a3f5e6d26ea363889778f
Anna Aneychik <anna.aneychik@ridedott.com>
2021-02-26 22:33:21 +0100

    Cover errors from fetching breeds list and images

--- src/components/__tests__/dogs-viewer.tsx
+++ src/components/__tests__/dogs-viewer.tsx
@@ -9,31 +9,55 @@ import React from 'react';
 import { DOG_API_BASE_URL } from '../../api';
 import DogsViewer from '../dogs-viewer';
 
-const server = setupServer(
-  // This is an unused parameter, it MUST be prefixed with underscore to compile
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  rest.get(`${DOG_API_BASE_URL}/breeds/list/all`, (_req, res, ctx) =>
-    res(
-      ctx.json({
-        status: 'success',
-        message: {
-          australian: ['shepherd'],
-          beagle: [],
-        },
-      }),
+// The unused `req` parameters MUST be prefixed with underscore to compile
+const handlers = {
+  getBreedsList:
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    rest.get(`${DOG_API_BASE_URL}/breeds/list/all`, (_req, res, ctx) =>
+      res(
+        ctx.json({
+          status: 'success',
+          message: {
+            australian: ['shepherd'],
+            beagle: [],
+          },
+        }),
+      ),
     ),
-  ),
-  // This is an unused parameter, it MUST be prefixed with underscore to compile
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  rest.get(`${DOG_API_BASE_URL}/breed/*/images/random/*`, (_req, res, ctx) =>
-    res(
-      ctx.json({
-        status: 'success',
-        message: ['image-url-1.jpg', 'image-url-2.jpg'],
-      }),
+  getImagesForBreed:
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    rest.get(`${DOG_API_BASE_URL}/breed/*/images/random/*`, (_req, res, ctx) =>
+      res(
+        ctx.json({
+          status: 'success',
+          message: ['image-url-1.jpg', 'image-url-2.jpg'],
+        }),
+      ),
     ),
-  ),
-);
+};
+
+const erroringHandlers = {
+  getBreedsList:
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    rest.get(`${DOG_API_BASE_URL}/breeds/list/all`, (_req, res, ctx) =>
+      res(
+        ctx.json({
+          status: 'error',
+        }),
+      ),
+    ),
+  getImagesForBreed:
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    rest.get(`${DOG_API_BASE_URL}/breed/*/images/random/*`, (_req, res, ctx) =>
+      res(
+        ctx.json({
+          status: 'error',
+        }),
+      ),
+    ),
+};
+
+const server = setupServer(...Object.values(handlers));
 
 beforeAll(() => {
   server.listen({ onUnhandledRequest: 'error' });
@@ -80,6 +104,33 @@ test('fetches images if breed is provided', async () => {
     expect(screen.getByTestId('breeds-loaded')).toBeInTheDocument();
     expect(screen.getAllByAltText(/beagle/iu)).not.toHaveLength(0);
   });
+});
+
+test('fetches images for one-word breed even if all breeds failed to load', async () => {
+  server.resetHandlers(
+    erroringHandlers.getBreedsList,
+    handlers.getImagesForBreed,
+  );
+  render(<DogsViewer breed="akita" />);
+
+  expect(screen.getByText(/more pictures for you/iu)).toBeVisible();
+
+  await waitFor(() => {
+    expect(screen.getAllByAltText(/akita/iu)).not.toHaveLength(0);
+  });
+});
+
+test('shows error if could not fetch images for breed', async () => {
+  server.resetHandlers(
+    handlers.getBreedsList,
+    erroringHandlers.getImagesForBreed,
+  );
+  render(<DogsViewer breed="beagle" />);
+
+  const error = await screen.findByText(
+    "Sorry, couldn't load images for beagle",
+  );
+  expect(error).toBeVisible();
 });
 
 test('image gallery is accessible', async () => {
