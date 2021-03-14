099fd96e04f40d93edc50b605f7217aababdde76
Anna Aneychik <anna.aneychik@ridedott.com>
2021-03-14 18:06:12 +0100

    Increase timeout for classifying the image in tests

--- cypress/e2e/app.ts
+++ cypress/e2e/app.ts
@@ -7,7 +7,7 @@ describe('app', () => {
     cy.findByRole('progressbar', { name: 'Classifying...' }).should(
       'be.visible',
     );
-    cy.findByText(/Looks like a/iu)
+    cy.findByText(/Looks like a/iu, { timeout: 15000 })
       .should('be.visible')
       .contains('beagle');
     cy.findAllByAltText('beagle').should('have.length.at.least', PAGE_SIZE);
