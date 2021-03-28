c970a22199a4c6fa07880ef61195fb2a0af4868f
Anna Aneychik <anna.aneychik@ridedott.com>
2021-03-28 11:19:44 +0200

    Trying to fix the github action for code coverage

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,6 @@ const plugins = ['@babel/plugin-proposal-optional-chaining'];
 
 if (process.env.CODE_COVERAGE === 'true') {
   plugins.push('istanbul');
-  console.log('ATTENTION: istabul included');
-} else {
-  console.log('ATTENTION: istanbul not included');
 }
 
 module.exports = {
