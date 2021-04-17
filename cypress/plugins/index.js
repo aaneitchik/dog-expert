e0c96152c7ef69f7a77d4a0fc471faf6f54966ed
Anna Aneychik <anna.aneychik@ridedott.com>
2021-04-17 21:11:40 +0200

    Try out Cypress Component Testing
    * Replace tests with too many mocked browser APIs with Cypress Component Tests (CCT)
    * Merge CCT code coverage with other reports

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,14 @@
 module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config);
-  // include any other plugin code...
+
+  if (config.testingType === 'component') {
+    const { startDevServer } = require('@cypress/webpack-dev-server');
+    const webpackConfig = require('../../webpack.dev.config');
+
+    on('dev-server:start', (options) =>
+      startDevServer({ options, webpackConfig }),
+    );
+  }
 
   // It's IMPORTANT to return the config object
   // with any changed environment variables
